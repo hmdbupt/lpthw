e44d5a5f0e777340e4c13c8d57a66d43af09f106
Hammad <hmdbupt@yahoo.com>
2019-10-01 04:37:11 +0800

    Exercises 1,2 and 3 from learn python the hard way

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# Prints I will now count my chickens:
+print("I will now count my chickens:")
+
+# Prints the result of calculation for the number of Hens
+print("Hens", 25.0+30.0/6.0)
+# Prints the result of calculation for the number of Roosters
+print("Roosters",100.0-25.0*3.0%4.0)
+
+# Prints Now I will count the eggs:
+print("Now I will count the eggs:")
+
+# Does the calculation and prints on the screen
+print(3.0+2.0+1-5.0+4.0%2.0-1.0/4.0+6.0)
+
+# Prints as it is on the screen
+print("Is it true that 3+2<5-7?")
+
+# Compares the values and prints the answer on the screen
+print(3.0+2.0<5.0-7.0)
+
+# Calculates the value of 3+2
+print("What is 3+2?",3.0+2.0)
+# Calculates the value of 5-7
+print("What is 5-7?",5.0-7.0)
+
+# Prints whatever is inside the double-quotes on the screen
+print("Oh, that's why it's False.")
+print("How about some more.")
+
+# Prints the answer for the questions in true and false format
+print("Is it greater?", 5.0>-2.0)
+print("Is it greater or equal?", 5.0>=-2.0)
+print("Is it less or equal?",5.0<=-2.0)
