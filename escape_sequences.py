501110450e224a00f7617a3a0a114daad88da1ad
Hammad <hmdbupt@yahoo.com>
2019-10-01 13:49:51 +0800

    Create escape_sequences.py
    List of escape sequences

---
+++ escape_sequences.py
@@ -0,0 +1,18 @@
+print('''
+Escape sequences
+The \\\\ Backslash
+The \\\' Single-quote
+The \\\" Double-quote
+The \\a ASCII bell - BEL
+The \\b ASCII backspace - BS
+The \\f ASCII formfeed - FF
+The \\n ASCII linefeed - LF
+The \\N{name} Character named name in the Unicode database
+The \\r Carriage return
+The \\t Horizontal tab
+The \\v Vertical tab
+The \\uxxxx Character with 16-bit hex value xxxx
+The \\Uxxxxxxxx Character with 32-bit hex value xxxxxxxx
+The \\000 Character with octal value 000
+The \\xhh Character with hex value hh
+''')
