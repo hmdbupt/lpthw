eb9ee7e31bb6656623c0b9689a4090e1d4619d9f
Hammad <hmdbupt@yahoo.com>
2019-10-01 20:42:51 +0800

    Update ex11_information_form.py
    Removed the input statement with prompts and used print -> input method instead

--- ex11_information_form.py
+++ ex11_information_form.py
@@ -1,7 +1,12 @@
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-age = input("Enter your age: ")
-email = input("Enter your email address: ")
+# the end = ' ' in this line lets you get the input on the same line
+print("Enter your first name:", end = ' ')
+first_name = input()
+print("Enter your last name:", end = ' ')
+last_name = input()
+print("Enter your age:", end = ' ')
+age = input()
+print("Enter your email address:", end = ' ')
+email = input()
 
 state = "Your name is {} {} \nYour age is {} \nYour email address is {}"
 print(state.format(first_name, last_name, age, email))
