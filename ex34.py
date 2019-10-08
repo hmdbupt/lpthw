d536299165109873348e992753564cabdba17299
Hammad <hmdbupt@yahoo.com>
2019-10-08 21:47:40 +0800

    Exercise 34 created

---
+++ ex34.py
@@ -0,0 +1,7 @@
+while True:
+    print("Choose a number between 0 and 5: ")
+    number = int(input("> "))
+
+    animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+    print(animals[number])
